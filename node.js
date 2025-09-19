4b5952c8d7d7f18abd16c25b993eeaddf697ddd9
yasaswinisai-annapurna <yasaswinisai06@gmail.com>
2025-09-19 13:43:11 +0530

    version-1

---
+++ node.js
@@ -0,0 +1,7 @@
+function addToCart(itemName) {
+  alert("✅ " + itemName + " has been added to your cart!");
+}
+
+function viewDetails(link) {
+  window.open(link, "_blank"); // opens product website in new tab
+}
